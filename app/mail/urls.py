30efba730ef905b3af78bfe1ee4ab2affc0e4b24
Eruck Emmanuel <eruck@vehseh.com>
2023-09-11 22:38:48 +0100

    Added events endpoint

--- app/mail/urls.py
+++ app/mail/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from mail.views import MailBoxAPIView, MailBoxFolders, MailMessageAPIView
+from mail.views import MailBoxAPIView, MailBoxFolders, MailMessageAPIView, MailBoxEventAPIView
 
 urlpatterns = [
     path('api/v1/mail/', MailBoxAPIView.as_view()),
     path('api/v1/messages/', MailMessageAPIView.as_view()),
-    path('api/v1/folders/', MailBoxFolders.as_view())
+    path('api/v1/folders/', MailBoxFolders.as_view()),
+    path('api/v1/event/', MailBoxEventAPIView.as_view())
 ]
 
--- app/mail/views.py
+++ app/mail/views.py
@@ -1,7 +1,12 @@
+import logging
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 from mail.utils.api import get_mails, get_folders, get_message_details
 from mail.utils.sample import data
+
+logger = logging.getLogger(__name__)
 
 
 class MailBoxAPIView(APIView):
@@ -23,3 +28,17 @@ class MailBoxFolders(APIView):
 
     def get(self, request):
         return Response(get_folders(request.user))
+
+
+class MailBoxEventAPIView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        logger.info(f"{request.data}", 'the data')
+        print(f"\n\n {request.data}", 'the data')
+        return Response({"status": "success"})
+
+    def get(self, request):
+        logger.info(f"{request.query_params}", 'the data')
+        print(f"\n\n{request.query_params}", 'the data')
+        return Response({"status": "success"})
