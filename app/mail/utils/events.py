d4d721e75ebad3e6e2e89598b06ebe2a23b70c03
Eruck Emmanuel <eruck@vehseh.com>
2023-09-14 20:54:53 +0100

    Removed unseen during saving event

--- app/mail/utils/events.py
+++ app/mail/utils/events.py
@@ -14,8 +14,7 @@ def save_dovecot_push_event(data: dict) -> None:
     EmailEvent.objects.create(
         uid=data.get('uid'), uid_validity=data.get('uid-validity'),
         subject=data.get('subject'), user=data.get('user'), snippet=data.get('snippet'),
-        folder=data.get('folder'), sender=data.get('sender'), event=data.get('event'),
-        unseen=data.get('unseen'))
+        folder=data.get('folder'), sender=data.get('sender'), event=data.get('event'))
 
 
 def process_dovecot_push_event(event_id: int) -> None:
