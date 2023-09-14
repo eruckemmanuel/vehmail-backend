4746c562b042683412daf6e479413fed5e73c0ae
Eruck Emmanuel <eruck@vehseh.com>
2023-09-14 20:48:20 +0100

    Fixed URL path

--- app/mail/urls.py
+++ app/mail/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('api/v1/mail/', MailBoxAPIView.as_view()),
     path('api/v1/messages/', MailMessageAPIView.as_view()),
     path('api/v1/folders/', MailBoxFolders.as_view()),
-    path('api/v1/event/<str: event_token>', MailBoxEventAPIView.as_view())
+    path('api/v1/event/<str:event_token>', MailBoxEventAPIView.as_view())
 ]
 
